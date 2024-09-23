bd8255cdcdca31064b5af458bea3e95fded28cb7
willymateo <wjmateo@espol.edu.ec>
2024-09-22 22:03:07 -0500

    Setup project

---
+++ locksmith/env.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+import environ
+
+env = environ.Env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
---
+++ locksmith/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for locksmith project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+from django.core.wsgi import get_wsgi_application
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "locksmith.django.development")
+
+application = get_wsgi_application()
