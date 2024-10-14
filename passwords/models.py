88ce6e9fbb952b220c131b7fc6529a0271cc6b20
willymateo <wjmateo@espol.edu.ec>
2024-10-13 23:37:19 -0500

    Create class based views

--- passwords/models.py
+++ passwords/models.py
@@ -6,6 +6,9 @@ class PasswordCategory(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     name = models.CharField(max_length=255, unique=True, null=False, blank=False)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
@@ -18,9 +21,13 @@ class Password(models.Model):
     category = models.ForeignKey(
         PasswordCategory,
         on_delete=models.PROTECT,
+        related_name="passwords",
         null=True,
         db_column="password_category_id",
     )
+
+    class Meta:
+        ordering = ["category", "name"]
 
     def __str__(self):
         return self.name, self.username
--- passwords/serializers.py
+++ passwords/serializers.py
@@ -3,13 +3,26 @@ from rest_framework import serializers
 from passwords.models import Password, PasswordCategory
 
 
-class PasswordSerializer(serializers.HyperlinkedModelSerializer):
+class PasswordCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PasswordCategory
+        fields = "__all__"
+
+
+class PasswordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Password
         fields = ["id", "name", "username", "password", "category"]
+        depth = 1
 
+    def create(self, validated_data):
+        password_category_id = validated_data.pop("password_category_id")
+        password_category = PasswordCategory.objects.get(pk=password_category_id)
 
-class PasswordCategorySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = PasswordCategory
-        fields = ["id", "name"]
+        return Password.objects.create(**validated_data, category=password_category)
+
+    def update(self, instance, validated_data):
+        password_category_id = validated_data.pop("password_category_id")
+        password_category = PasswordCategory.objects.get(pk=password_category_id)
+
+        return Password.objects.update(**validated_data, category=password_category)
